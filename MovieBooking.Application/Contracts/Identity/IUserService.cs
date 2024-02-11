c5bff05789d2bf0a26890396fa5c2ddc9fefe8aa
nishar <nishar@NISHA-RATHOD>
2024-02-11 20:04:07 +0530

    Changes In User Module

--- MovieBooking.Application/Contracts/Identity/IUserService.cs
+++ MovieBooking.Application/Contracts/Identity/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using MovieBooking.Application.Features.Common;
+﻿using MovieBooking.Application.Contracts.Responses;
+using MovieBooking.Application.Features.Common;
+using MovieBooking.Application.Interfaces;
 using MovieBooking.Application.Models.Users;
 using System;
 using System.Collections.Generic;
@@ -8,10 +10,13 @@ using System.Threading.Tasks;
 
 namespace MovieBooking.Application.Contracts.Identity
 {
-    public interface IUserService
+    public interface IUserService : ITransientService
     {
         Task<ApiResponse<UserDetailsDto>> GetUserDetailsAsync(string userId, CancellationToken cancellationToken);
         //Task<List<string>> GetPermissionAsync(string userId, CancellationToken cancellationToken);
         //Task<bool> HasPermissionAsync(string? userId, string permission, CancellationToken cancellationToken = default);
+        Task<ApiResponse<string>> UpdateAsync(UpdateUserRequest request);
+        Task<ApiResponse<string>> DeleteAsync(string userId);
+        Task<IPagedDataResponse<UserListDto>> SearchAsync(UserListFilter filter, CancellationToken cancellationToken);
     }
 }
---
+++ MovieBooking.Application/Models/Users/UserListFilter.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using MovieBooking.Application.Models.Specification;
+using MovieBooking.Application.Models.Specification.Filters;
+
+namespace MovieBooking.Application.Models.Users;
+public class UserListFilter : PaginationFilter
+{
+}
+
+public class GetSearchUserRequestSpec : EntitiesByPaginationFilterSpec<UserListDto>
+{
+    public GetSearchUserRequestSpec(UserListFilter request)
+    : base(request) =>
+        Query.OrderByDescending(c => c.Id, !request.HasOrderBy());
+}
